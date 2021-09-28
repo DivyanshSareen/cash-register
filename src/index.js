937906ff563d7ea5487722318c2c9e2d596b2b1f
DivyanshSareen <59335572+DivyanshSareen@users.noreply.github.com>
2021-09-28 15:56:33 +0530

    added comments in index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import "./styles.css";
 
+// getting all the DOM elements
 var bill = document.getElementById("bill");
 var paid = document.getElementById("paid");
 var calc = document.getElementById("calc");
 var data = document.getElementById("data");
+
 calc.addEventListener("click", handleClick);
-var cash_init = {
-  2000: 0,
-  500: 0,
-  100: 0,
-  20: 0,
-  10: 0,
-  5: 0,
-  1: 0
-};
+
+// calculated values will be stored here for display
 var change = {
   2000: 0,
   500: 0,
@@ -24,8 +19,10 @@ var change = {
   1: 0
 };
 
+// array of available notes
 const note = [2000, 500, 100, 20, 10, 5, 1];
 
+// displays 'change' values for each note
 data.innerHTML = `<th>No.</th>
 <td>${change[note[0]]}</td>
 <td>${change[note[1]]}</td>
@@ -35,18 +32,23 @@ data.innerHTML = `<th>No.</th>
 <td>${change[note[5]]}</td>
 <td>${change[note[6]]}</td>`;
 
+// callback function for onclick event on calc button
 function handleClick() {
   let bal = parseInt(paid.value, 10) - parseInt(bill.value, 10);
-  change = cash_init;
+
+  // intializing all the change values to 0
   for (let i = 0; i < note.length; i++) {
     change[note[i]] = 0;
   }
+  // calculating minimum number of notes to get the balance amount
   for (let i = 0; i < note.length; i++) {
     if (bal >= note[i]) {
       change[note[i]] = Math.floor(bal / note[i]);
       bal = bal - Math.floor(change[note[i]] * note[i]);
     }
   }
+
+  // displaying the new values of change object
   data.innerHTML = `<th>No.</th>
 <td>${change[note[0]]}</td>
 <td>${change[note[1]]}</td>
